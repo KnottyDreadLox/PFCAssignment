8ccfab5c6aa0f4ae8d188e177e13833790bfe718
Lenny Bruno <67169689+KnottyDreadLox@users.noreply.github.com>
2022-03-29 14:51:08 +0200

    Base for login

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ import {
   AddNewUser
 } from "./db.js";
 
-const PORT = 80;
+const DEV = true;
+const PORT = DEV ? 80 : 443;
 const SECRET_MANAGER_CERT =
   "projects/3469417017/secrets/PublicKey/versions/latest";
 const SECRET_MANAGER_PK =
@@ -43,16 +44,49 @@ const startServerEncrypted = async () => {
   });
 };
 
-const startServer = () => {
-  app.listen(PORT, () => console.log("Server Listening on port: " + PORT));
+
+export let PDF_API_KEY = "";
+
+const startServer = async () => {
+  //Load GetOutPDF API Key
+  const [pdf] = await sm.accessSecretVersion({
+    name: SECRET_MANAGER_GET_OUT_PDF,
+  });
+  PDF_API_KEY = pdf.payload.data.toString();
+  if (!DEV) {
+    const [pub] = await sm.accessSecretVersion({
+      name: SECRET_MANAGER_CERT,
+    });
+
+    const [prvt] = await sm.accessSecretVersion({
+      name: SECRET_MANAGER_PK,
+    });
+
+    const sslOptions = {
+      key: prvt.payload.data.toString(),
+      cert: pub.payload.data.toString(),
+    };
+
+    https.createServer(sslOptions, app).listen(PORT, () => {
+      console.log("Secure Server Listening on port:" + PORT);
+    });
+  } else {
+    app.listen(PORT, () => console.log("Server Listening on port: " + PORT));
+  }
 };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = Express();
-//enabled http -> https redirection
-//app.enable("trust proxy");
+
+
+if (!DEV) {
+  app.enable("trust proxy");
+  app.use((req, res, next) => {
+    req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+  });
+}
 
 //serve static files
 app.use(Express.static(path.join(__dirname, "../frontend/public")));
@@ -82,7 +116,7 @@ app.get("/account", (req, res) => {
   }).catch(e => console.log(e));
 
   res.sendFile(path.join(__dirname, "../frontend/account.html"));
-});2
+});
 
 
 app.get("/convert", (req, res) => {
